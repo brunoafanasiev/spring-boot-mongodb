9216924ebf90f19a8f0b79e54de808a83af0983a
Bruno Afanasiev <b.afanasiev@gmail.com>
2019-06-25 22:18:46 -0300

    Conectando no mongoDB

--- src/main/java/com/curso/mongodb/resources/UserResources.java
+++ src/main/java/com/curso/mongodb/resources/UserResources.java
@@ -1,28 +1,28 @@
 package com.curso.mongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.mongodb.domain.User;
+import com.curso.mongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria  = new User("1","maria silva","Maria@uol.com.br");
-		User alex  = new User("2","Alex Lima","alex@terra.com.br");
 		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
-		
+		List<User> list = userService.findAll();
+				
 		return ResponseEntity.ok().body(list);
 	}
 }
